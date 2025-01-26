5857cf7177cb98ba64368aa6fdd2098fa2d425c4
Daniel <daniel.rod.moreno@gmail.com>
2025-01-26 21:00:05 +0100

    add Material-UI and Emotion dependencies; update App component layout for medication search

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="page">
+
+    <header>
+      <h1>Buscador de Medicamentos</h1>
+     <form className="form">
+        <input type="text" placeholder="Search... " />
+        <button type="submit">Search</button>
+      </form>
+    </header>
+      
+    <main>
+      Resultados
+    </main>
+    </div>
   )
 }
-
+//https://api.fda.gov/drug/label.json?search=openfda.generic_name:%22Aspirin%22
 export default App
